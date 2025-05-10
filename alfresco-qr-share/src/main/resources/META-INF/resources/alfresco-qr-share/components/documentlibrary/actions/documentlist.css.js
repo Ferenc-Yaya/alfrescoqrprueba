f94be883e32a8f36db3752346bba3e2902bece6d
Ferenc <ferenc_yaya@hotmail.com>
2025-05-09 19:38:34 -0500

    cambiando iconos

--- alfresco-qr-share/src/main/resources/META-INF/resources/alfresco-qr-share/components/documentlibrary/actions/documentlist.css.js
+++
@@ -1,2 +0,0 @@
-/* Add custom action icons */
-document.write('<link rel="stylesheet" type="text/css" href="${url.context}/res/alfresco-qr-share/components/documentlibrary/actions/custom-actions.css" />');
