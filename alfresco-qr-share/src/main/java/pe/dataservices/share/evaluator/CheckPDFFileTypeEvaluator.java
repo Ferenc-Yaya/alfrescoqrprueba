554ee8c7748b3236012c9b5a4a079a426bdfae62
Ferenc <ferenc_yaya@hotmail.com>
2025-05-12 13:55:45 -0500

    qr funcionando

--- alfresco-qr-share/src/main/java/pe/dataservices/share/evaluator/CheckPDFFileTypeEvaluator.java
+++ alfresco-qr-share/src/main/java/pe/dataservices/share/evaluator/CheckPDFFileTypeEvaluator.java
@@ -1,20 +1,3 @@
-//package pe.dataservices.share.evaluator;
-//
-//import org.alfresco.web.evaluator.BaseEvaluator;
-//import org.json.simple.JSONObject;
-//
-//public class CheckPDFFileTypeEvaluator extends BaseEvaluator {
-//    @Override
-//    public boolean evaluate(JSONObject jsonObject) {
-//        try {
-//            String mimetype = getNodeMimetype(jsonObject);
-//            return mimetype != null && mimetype.equals("application/pdf");
-//        } catch (Exception e) {
-//            return false;
-//        }
-//    }
-//}
-
 package pe.dataservices.share.evaluator;
 
 import org.alfresco.web.evaluator.BaseEvaluator;
@@ -29,13 +12,9 @@ public class CheckPDFFileTypeEvaluator extends BaseEvaluator {
     public boolean evaluate(JSONObject jsonObject) {
         try {
             String mimetype = getNodeMimetype(jsonObject);
-            logger.error("CheckPDFFileTypeEvaluator - Evaluando nodo con mimetype: " + mimetype);
+            logger.debug("CheckPDFFileTypeEvaluator - Evaluando nodo con mimetype: " + mimetype);
 
-            // Para pruebas, siempre devuelve true
-            return true;
-
-            // Código original:
-            // return mimetype != null && mimetype.equals("application/pdf");
+            return mimetype != null && mimetype.equals("application/pdf");
         } catch (Exception e) {
             logger.error("CheckPDFFileTypeEvaluator - Error al evaluar el tipo de archivo", e);
             return false;
